103bce888e8463af00275ffe625779a2bbf7c2cd
Harrison <nhcdeveloper@gmail.com>
2023-03-29 18:10:50 +0100

    Astro github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://harricon-nc.github.io",
+  base: "/fem_loopstudios-landing-page"
+});
